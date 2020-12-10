7ad01e12aaf55ce41bf38ca91930873e6f39eb69
josh <joshuarvlcb@gmail.com>
2020-12-10 15:26:20 -0700

    finishing sign up

---
+++ sign-up/hire.js
@@ -0,0 +1,19 @@
+const navBar = document.getElementById("navbar");
+
+function navButton() {
+    navBar.classList.add("showNav");
+
+    if (navBar == navBar) {
+        document.getElementById('nav-button').classList.remove("nav-button");
+
+    }
+}
+
+function navClose() {
+    navBar.classList.remove("showNav")
+
+    if (
+        navBar.classList.remove("showNav") == navBar.classList.remove("showNav")) {
+        document.getElementById("nav-button").classList.add("nav-button");
+    }
+}
