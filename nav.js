fd6ae2c63ea5f50d4510ecbcc2ccaad2d7e4ffb0
josh <joshuarvlcb@gmail.com>
2020-12-11 19:28:02 -0700

    made the nav bar cool

--- nav.js
+++ nav.js
@@ -1,19 +1,27 @@
-const navBar = document.getElementById("navbar");
 
-function navButton() {
-    navBar.classList.add("showNav");
+const navBar = document.getElementById("nav-button");
 
-    if (navBar == navBar) {
-        document.getElementById('nav-button').classList.remove("nav-button");
+const navLinks = document.getElementById("navbar");
+
+
+const burger = document.getElementById('bars')
+
+let menuOpen = false;
+
+function navButton () {
+    if(!menuOpen){
+        navBar.classList.add('open')
+        menuOpen = true;
+        navLinks.classList.add("showNav");
+
+
+    }else{
+        navBar.classList.remove("open");
+        menuOpen = false;
+        navLinks.classList.remove("showNav");
 
     }
+
 }
 
-function navClose() {
-    navBar.classList.remove("showNav")
 
-    if (
-        navBar.classList.remove("showNav") == navBar.classList.remove("showNav")) {
-        document.getElementById("nav-button").classList.add("nav-button");
-    }
-}
